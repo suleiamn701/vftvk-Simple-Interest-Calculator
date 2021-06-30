13d0a7b00a9a014c62102487f007032bf7790b38
suleiamn701 <s125388@student.squ.edu.om>
2021-06-30 22:33:55 +0400

    work done

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+    total = (p * r *y )/1000
+    years = y +2020
+
+    document.getElementById("result").innerHTML = "if you despoiy "+p+" at in interst rate of "+r+" You will revice "+ total+" in the year "+years;
+   
 }
         
